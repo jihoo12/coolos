55fb4b4ec34e2a00cebebceb571e4ea4d069f855
jihoo <jihoop0620@gmail.com>
2026-01-27 11:56:14 +0900

    backup

--- run.sh
+++ run.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 set -e
 
-# Compile
-# Actually, the user's run.sh might have a hardcoded clang command or just call make.
-# Looking at the previous run.sh view, line 5 is 'make'.
-# But the log showed a long clang command.
-# Let me just ensure Makefile is enough.
+# 1. 빌드 (Make 실행)
 make
 
-# Create directory structure
+# 2. EFI 실행 파일 배치를 위한 디렉토리 생성 및 복사
 mkdir -p boot/EFI/BOOT
-
-# Copy the EFI application
 cp main.efi boot/EFI/BOOT/BOOTX64.EFI
 
-# Run in QEMU
-# -bios: specify the UEFI firmware
-# -drive file=fat:rw:boot: use the 'boot' directory as a virtual FAT drive
-# Remove -nographic to see the GOP output (requires X11/Wayland)
-qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -drive file=fat:rw:boot,format=raw -vga std -drive file=nvme.img,if=none,id=mynvme,format=raw -device nvme,serial=coolos_nvme,drive=mynvme
+# 3. NVMe 이미지 파일이 없을 경우 생성 (128MB 크기 예시)
+# qemu-img 명령어를 사용하여 가상 디스크 파일을 만듭니다.
+if [ ! -f nvme.img ]; then
+    echo "Creating nvme.img..."
+    qemu-img create -f raw nvme.img 128M
+fi
+
+# 4. QEMU 실행
+# -bios: UEFI 펌웨어 지정
+# -drive file=fat:rw:boot: 'boot' 폴더를 가상 FAT 드라이브로 사용
+# -drive ... nvme.img: 생성한 NVMe 이미지를 연결
+qemu-system-x86_64 \
+    -bios /usr/share/ovmf/OVMF.fd \
+    -drive file=fat:rw:boot,format=raw \
+    -vga std \
+    -drive file=nvme.img,if=none,id=mynvme,format=raw \
+    -device nvme,serial=coolos_nvme,drive=mynvme
